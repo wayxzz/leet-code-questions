2fb6098ef8d7e2c559873a44c968076aa695d674
wayxz <niewei502533466@gmail.com>
2018-05-28 11:37:42 +0800

    完成第三题

--- questions/3_longest-substring-without-repeating-characters.js
+++ questions/3_longest-substring-without-repeating-characters.js
@@ -4,11 +4,45 @@
  * 
  * 示例：
  * 
- * 给定&nbsp;&quot;abcabcbb&quot;&nbsp;，没有重复字符的最长子串是&nbsp;&quot;abc&quot;&nbsp;，那么长度就是3。
+ * 给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
  * 
- * 给定&nbsp;&quot;bbbbb&quot;&nbsp;，最长的子串就是&nbsp;&quot;b&quot;&nbsp;，长度是1。
+ * 给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
  * 
- * 给定&nbsp;&quot;pwwkew&quot;&nbsp;，最长子串是&nbsp;&quot;wke&quot;&nbsp;，长度是3。请注意答案必须是一个子串，&quot;pwke&quot;&nbsp;是&nbsp;子序列&nbsp;&nbsp;而不是子串。
+ * 给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列  而不是子串。
  * 
  *
  */
+
+ /**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function(s) {
+    
+    if(typeof s !== 'string') {
+        return 0;
+    }
+    let length = s.length;
+    if(length < 2) {
+        return length;
+    }
+
+    let start = 0,
+        temp = '',
+        result = '';
+
+    for(let i = 0; i < length; i++) {
+        let index = temp.indexOf(s[i]);
+
+        if(index > -1) {
+            start = start + index + 1;
+        }
+        temp = s.substring(start, i + 1);
+        if(result.length < temp.length) {
+            result = temp;
+        }
+    }
+    return result.length;
+};
+
+module.exports = lengthOfLongestSubstring;
--- test/3_longest-substring-without-repeating-characters.test.js
+++ test/3_longest-substring-without-repeating-characters.test.js
@@ -4,11 +4,62 @@
  * 
  * 示例：
  * 
- * 给定&nbsp;&quot;abcabcbb&quot;&nbsp;，没有重复字符的最长子串是&nbsp;&quot;abc&quot;&nbsp;，那么长度就是3。
+ * 给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
  * 
- * 给定&nbsp;&quot;bbbbb&quot;&nbsp;，最长的子串就是&nbsp;&quot;b&quot;&nbsp;，长度是1。
+ * 给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
  * 
- * 给定&nbsp;&quot;pwwkew&quot;&nbsp;，最长子串是&nbsp;&quot;wke&quot;&nbsp;，长度是3。请注意答案必须是一个子串，&quot;pwke&quot;&nbsp;是&nbsp;子序列&nbsp;&nbsp;而不是子串。
+ * 给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列  而不是子串。
  * 
  *
  */
+
+ const lengthOfLongestSubstring = require("../questions/3_longest-substring-without-repeating-characters");
+
+test('The longest substring without repeating characters of string "" is 0', () => {
+    const str = '';
+    const result = 0;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "abcabcbb" is 3', () => {
+    const str = 'abcabcbb';
+    const result = 3;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "bbbbb" is 1', () => {
+    const str = 'bbbbb';
+    const result = 1;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "pwwkew" is 3', () => {
+    const str = 'pwwkew';
+    const result = 3;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "au" is 2', () => {
+    const str = 'au';
+    const result = 2;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "dvdf" is 3', () => {
+    const str = 'dvdf';
+    const result = 3;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
+
+test('The longest substring without repeating characters of string "abcabaabcd" is 4', () => {
+    const str = 'abcdabcaba';
+    const result = 4;
+
+    expect(lengthOfLongestSubstring(str)).toBe(result);
+})
